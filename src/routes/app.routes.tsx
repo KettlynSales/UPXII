c98d24d9db17989bc8bf352b1b4b2af8b65c26a4
KettlynSales <kettlynsales17@hotmail.com>
2024-05-15 19:26:06 -0300

    feat: removendo header

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,25 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
 
 import { Home } from "@screens/Home";
 import { Doacoes } from "@screens/Doacoes";
 import { Profile } from "@screens/Profile";
 
-const { Navigator, Screen } = createBottomTabNavigator();
+type AppRoutes = {
+  home: undefined;
+  doacoes: undefined;
+  profile: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="doacoes" component={Doacoes} />
       <Screen name="profile" component={Profile} />
