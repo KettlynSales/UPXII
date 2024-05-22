7d33158283db5c67b631ae2103d2f0e881cf2142
KettlynSales <kettlynsales17@hotmail.com>
2024-05-22 17:40:16 -0300

    feat: home quando for perfil do usuario

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,75 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { VStack, FlatList, Heading } from "native-base";
+import { InstitutionCard } from "@components/InstitutionCard";
+import { useNavigation } from "@react-navigation/native";
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import React = require("react");
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    "roupas",
+    "alimentos",
+    "brinquedos",
+    "móveis",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("roupas");
+  const [institutions, setInstitutions] = useState([
+    "Alimentos",
+    "Roupas",
+    "Brinquedos",
+    "Móveis",
+  ]);
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenInstitutionDetails() {
+    navigation.navigate("institution");
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1} bg="white">
+      <HomeHeader />
+      <Heading
+        px={8}
+        mt={6}
+        fonts="heading"
+        color="gray.500"
+        size="sm"
+        fontFamily="bold"
+      >
+        Instituições que você pode ajudar com...
+      </Heading>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+        mb={4}
+      />
+      <VStack flex={1} px={8}>
+        <FlatList
+          data={institutions}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <InstitutionCard onPress={handleOpenInstitutionDetails} />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
+    </VStack>
   );
 }
