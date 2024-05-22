a15150b650c44d2cd6f07ae442a48b362e88f281
KettlynSales <kettlynsales17@hotmail.com>
2024-05-22 17:33:59 -0300

    feat: tela de signIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,13 @@
-import { VStack, Center, Text, Heading, ScrollView } from "native-base";
+import React = require("react");
+
+import { VStack, Center, Text, Heading, ScrollView, Image } from "native-base";
 
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 import { useNavigation } from "@react-navigation/native";
+import Logo from "@assets/logo.png";
 
 export function SignIn() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
@@ -18,14 +21,10 @@ export function SignIn() {
       contentContainerStyle={{ flexGrow: 1 }}
       showsVerticalScrollIndicator={false}
     >
-      <VStack flex={1} bg="coolGray.700" px={10} pb={16}>
-        <Center my={24}>
-          <Text color="gray.100" fontSize="lg">
-            UPX Doações
-          </Text>
-        </Center>
-        <Center>
-          <Heading color="gray.100" mb={6} fontFamily="heading">
+      <VStack flex={1} bg="yellow.700" px={10}>
+        <Center my={20}>
+          <Image source={Logo} w={180} h={180} alt="logo" />
+          <Heading color="pink.500" mb={6} fontFamily="heading">
             Acesse sua conta
           </Heading>
           <Input
@@ -36,8 +35,8 @@ export function SignIn() {
           <Input placeholder="Senha" secureTextEntry />
           <Button title="Acessar conta" />
         </Center>
-        <Center mt={24}>
-          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+        <Center>
+          <Text color="pink.500" fontSize="sm" mb={3} fontFamily="body">
             Ainda não tenho acesso
           </Text>
           <Button
